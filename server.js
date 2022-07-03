836b33fd79440bcc46553ea86b41bc0f2b27b118
Dekota Nelson <nelson.dekota@gmail.com>
2022-07-03 17:24:40 -0400

    updated db conections

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require('express')
 const app = express()
-const connectDB = require("./config/database")
 const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
 require('dotenv').config()
 
 
-// let db,
-//     dbConnectionStr = process.env.DB_STRING,
-//     dbName = 'meme-break'
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'meme-break'
 
-// MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-//     .then(client => {
-//         console.log(`Connected to ${dbName} Database`)
-//         db = client.db(dbName)
-//     })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
     
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -50,10 +49,6 @@ app.put('/addonelike', (request, response) => {
 
 })
 
-//Ensure DB connection then listen for requests
-connectDB().then(() => {
-    console.log("db connected");
-    app.listen(process.env.PORT, () => {
-        console.log("Server is running, you better catch it!");
-    })
-  })
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
