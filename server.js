65b90427df116c01185809c0bc80c857f4ddb5e4
Dekota Nelson <nelson.dekota@gmail.com>
2022-07-03 17:19:35 -0400

    updated db conections

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const app = express()
+const connectDB = require("./config/database")
 const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
 require('dotenv').config()
 
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'meme-break'
+// let db,
+//     dbConnectionStr = process.env.DB_STRING,
+//     dbName = 'meme-break'
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
-    })
+// MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+//     .then(client => {
+//         console.log(`Connected to ${dbName} Database`)
+//         db = client.db(dbName)
+//     })
     
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
